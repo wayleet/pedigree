7c365f2bd5a09ef7eeb7fdecec1640eb92cdb1db
Danilok95432 <102434452+Danilok95432@users.noreply.github.com>
2024-02-20 15:30:05 +0300

    #4 PhotoPreview (#11)
    * first_commit
    
    * second_commit
    
    * third_commit
    
    * forth_commit
    
    * fifth_commit
    
    * sixth_commit

--- vue/src/components/pages/Home.vue
+++ vue/src/components/pages/Home.vue
@@ -4,18 +4,21 @@
       Homepage
       <button @click="() => openHelpModal()">Open modal</button>
     </section>
+    <PhotoPreview size="large" photo="https://mykaleidoscope.ru/x/uploads/posts/2022-10/1666206241_12-mykaleidoscope-ru-p-kartinka-na-zastavku-oboi-12.jpg" />
   </PageLayout>
 </template>
 
 <script>
 import PageLayout from '../parts/PageLayout'
 import { helpModal } from "@/mixins/modals";
+import PhotoPreview from '../ui/PhotoPreview.vue';
 
 export default {
   name: 'HomePage',
   mixins: [helpModal],
   components: {
-    PageLayout
+    PageLayout,
+    PhotoPreview
   }
 }
 </script>
---
+++ vue/src/components/ui/PhotoPreview.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="photo" class="photo-preview">
+    <img class="photo-preview__img" :class="'photo-preview__img--' + size" :src="photo"  alt="">
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'PhotoPreview',
+  props: {
+    photo: {
+      default: null,
+      type: String
+    },
+    size: {
+      default: 'middle',
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.photo-preview {
+  &__img{
+    object-fit: cover;
+    border-radius: 12px;
+    &--large{
+      width: 200px;
+      height: 300px;
+    }
+
+    &--middle{
+      width: 150px;
+      height: 200px;
+    }
+
+    &--small{
+      width: 80px;
+      height: 120px;
+    }
+  }
+}
+</style>
