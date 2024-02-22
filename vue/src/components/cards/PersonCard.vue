f0ebc97a59e87be4f57be2fdb8908df5dbe87dd7
Viktoria Nikolaeva <110090587+liannl@users.noreply.github.com>
2024-02-22 14:29:02 +0300

    PersonCard (#18)
    * init PersonCard
    
    * xorrected errors and indentations
    
    * fixed headers
    
    * fixed h1 h2
    
    * fixed style
    
    ---------
    
    Co-authored-by: user <user@UbuntaWeb.myguest.virtualbox.org>

---
+++ vue/src/components/cards/PersonCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="person-card">
+    <h1>{{ fullName }}</h1>
+    <span class="person-card__dates">{{ person.birth_date }} </span>
+    <span v-if="person.die_date" class="person-card__dates"> - {{ person.die_date }}</span>
+
+    <h2>Родители</h2> 
+    <div class="person-card__information-text">-</div> 
+
+    <h2>Дети </h2>
+    <div class="person-card__information-text">-</div>
+    
+    <h2>Род деятельности</h2>
+    <div class="person-card__information-text">{{ person.activity }}</div>
+
+    <h2>Биография</h2>
+    <div class="person-card__information-text">{{ person.biography }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    person: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.person.secondName} ${this.person.firstName} ${this.person.patronymic}`;
+    }
+  }
+}
+</script>
+
+<style scoped lang ="less">
+.person-card {
+
+  &__dates {
+    font-size: 12px;
+    color: grey;
+    font-family:Arial;
+    padding-bottom:20px;
+  }
+  &__information-text {
+    font-weight: normal;
+    font-size: 10px;
+    padding-bottom:20px;
+    font-family:Arial;
+    color:black;
+  } 
+}
+
+h2 {
+  font-size: 14px;
+  font-weight: 700;
+  font-family:Arial;
+  color:black;
+}
+
+h1 {
+  font-size: 18px;
+  font-weight: 700;
+  font-family:Arial;
+  color:black;
+}
+</style>
--- vue/src/components/pages/Home.vue
+++ vue/src/components/pages/Home.vue
@@ -3,8 +3,9 @@
     <section class="p-16">
       Homepage
       <button @click="() => openHelpModal()">Open modal</button>
+      <PersonCard :person="person" />
     </section>
-
+    <PhotoPreview size="large" photo="https://mykaleidoscope.ru/x/uploads/posts/2022-10/1666206241_12-mykaleidoscope-ru-p-kartinka-na-zastavku-oboi-12.jpg" />
     <EducationForm :education="education" />
   </PageLayout>
 </template>
@@ -12,17 +13,37 @@
 <script>
 import PageLayout from '../parts/PageLayout'
 import { helpModal } from "@/mixins/modals";
+import PersonCard from '@/components/cards/PersonCard.vue';
+import PhotoPreview from '../ui/PhotoPreview.vue';
 import EducationForm from '../forms/EducationForm.vue';
+
+
 
 export default {
   name: 'HomePage',
   mixins: [helpModal],
   components: {
     PageLayout,
+    PhotoPreview,
+    PersonCard,
     EducationForm
+    
   },
-  data () {
+  data() {
     return {
+      person: {
+        id: 'gi6437tf63i4ftfo43f',
+        secondName: 'Иванов',
+        firstName: 'Иван',
+        patronymic: 'Иванович',
+        birth_date: '01.01.2000',
+        die_date: '',
+        gender: 'male',
+        children: ['y3g46yf34fy636b','3345fg35yg34f3f46'],
+        biography: 'Родился в ...',
+        activity: 'Бухгалтер',
+        photo: '/pictures/ivan.jpg'
+      },
       education: {
         type: 'Бакалавриат',
         level: 'Высшее',
@@ -31,7 +52,7 @@ export default {
         institutionName: 'ВГУ',
         institutionCity: ''
       }
-    }
+    };
   }
 }
 </script>
