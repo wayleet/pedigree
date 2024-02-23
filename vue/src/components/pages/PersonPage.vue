347c57b7ba5854521af696c3abc72e9140880b40
AntonioShb <40542681+Shabanov-Anton@users.noreply.github.com>
2024-02-24 00:36:37 +0300

    Added PersonPage #9 (#26)
    * Added PersonPage #9
    
    * Added name to PersonPage
    
    * Delete package-lock.json

---
+++ vue/src/components/pages/PersonPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ person.fullName }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonPage',
+  computed: {
+    person() {
+      // Здесь нужно получить объект человека по id из параметра страницы
+      return {
+        fullName: "Фамилия Имя Отчество" // Замените данными из объекта человека
+      };
+    },
+    id() {
+      return this.$route.params.id;
+    }
+  }
+};
+</script>
--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomePage from '../components/pages/Home.vue'
+import PersonPage from '../components/pages/PersonPage.vue'
 import { RouteNames } from './routes'
 
 Vue.use(VueRouter)
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: RouteNames.HOME,
     component: HomePage
+  },
+  {
+    path: '/person/:id',
+    name: 'PersonPage',
+    component: PersonPage
   }
 ]
 
