1340036f8e7b3a4553503462e18b56c1055fa325
reznikovkg <reznikov.kosta@yandex.ru>
2024-02-29 16:52:30 +0300

    refactoring Peson type

--- vue/src/types/person.ts
+++ vue/src/types/person.ts
@@ -1,57 +1,57 @@
-enum Gender {
+enum GENDER {
   MALE = "MALE",
   FEMALE = "FEMALE"
 }
 
 type PersonId = string;
-type Date = string;
+type DateString = string;
 
 interface Person {
-  id: PersonId;
-  secondName: string;                  // Фамилия
-  firstName: string;                   // Имя
-  patronymic: string;                  // Отчество
-  birth_date: Date;                    // Дата рождения
-  die_date: Date;                      // Дата смерти
-  gender: Gender;                      // Пол
-  biography: string;                   // Биография
-  activity: string;                    // Род деятельности
-  photo: string;                       // Ссылка на фото
-  children: PersonId[];                // Массив id детей
-  educations: Education[];             // Образования
-  weddings: Wedding[];                 // Брачные союзы
-  works: Work[];                       // Работы
+  id: PersonId; // id
+  secondName: string; // Фамилия
+  firstName: string; // Имя
+  patronymicName: string; // Отчество
+  birthDate: DateString; // Дата рождения
+  dieDate: DateString; // Дата смерти
+  gender: GENDER; // Пол
+  biography: string; // Биография
+  activity: string; // Род деятельности
+  photo: string; // Ссылка на фото
+  children: PersonId[]; // Массив id детей
+  educations: Education[]; // Образования
+  weddings: Wedding[]; // Брачные союзы
+  works: Work[]; // Работы
   militaryServices: MilitaryService[]; // Служба
 }
 
 interface Education {
-  type: string;            // Тип образования
-  level: string;           // Уровень образования
-  startDate: Date;         // Дата начала обучения
-  endDate: Date;           // Дата завершения обучения
-  institutionName: string; // Полное название образовательного учреждения
-  institutionCity: string; // Город
+  type: string; // Тип образования
+  level: string; // Уровень образования
+  startDate: DateString; // Дата начала обучения
+  endDate: DateString; // Дата завершения обучения
+  name: string; // Полное название образовательного учреждения
+  city: string; // Город
 }
 
 interface Wedding {
-  partnerId: PersonId; // Ссылка на партнера
-  date_start: Date;    // Дата заключения договора
-  date_end: Date;      // Дата расторжения брака
+  partnerId: PersonId; // id партнера
+  startDate: DateString; // Дата заключения договора
+  endDate: DateString; // Дата расторжения брака
 }
 
 interface Work {
-  countryCity: string;  // Город
+  place: string; // Страна, город
   organization: string; // Полное название организации
-  dateStart: Date;      // Дата начала работы
-  dataEnd: Date;        // Дата увольнения
-  jobTitle: string;     // Должность
-  description: string;  // Описание
+  startDate: DateString; // Дата начала работы
+  endDate: DateString; // Дата увольнения
+  position: string; // Должность
+  description: string; // Описание
 }
 
 interface MilitaryService {
-  type: string;        // Тип службы
-  rank: string;        // Звание, должность
-  date_start: Date;    // Дата начала службы
-  data_end: Date;      // Дата завершения службы
+  type: string; // Тип службы
+  rank: string; // Звание, должность
+  startDate: DateString; // Дата начала службы
+  endDate: DateString; // Дата завершения службы
   description: string; // Описание
 }
