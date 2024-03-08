ce63bf1f035a66458f44bdfbb6180f67dfaae601
vladenich1 <162311958+vladenich1@users.noreply.github.com>
2024-03-08 22:06:59 +0300

    #14 Раздел образование (#80)
    * First commit (add files to components and types)
    
    * deleted education.ts changed EducationItem.vue EducationList.vue
    
    ---------
    
    Co-authored-by: vladenich <Nichug202@yandex.ru>

---
+++ vue/src/components/parts/EducationItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>{{ education.type }} ({{ education.name }}) {{ education.startDate }} [{{ education.endDate }}]</li>
+</template>
+  
+<script>
+  export default {
+    name: 'EducationItem',
+    props: {
+      education: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+</script>
+  
+<style>
+</style>
+  
---
+++ vue/src/components/parts/EducationList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="education-list">
+    <h2>Образование</h2>
+    <ul>
+      <EducationItem v-for="(education, index) in educations" :key="index" :education="education" />
+    </ul>
+  </div>
+</template>
+  
+<script>
+  import EducationItem from './EducationItem.vue';
+  
+  export default {
+    name: 'EducationList',
+    components: {
+      EducationItem
+    },
+    props: {
+      educations: {
+        type: Array,
+        required: true
+      }
+    }
+  };
+</script>
+  
+<style>
+</style>
